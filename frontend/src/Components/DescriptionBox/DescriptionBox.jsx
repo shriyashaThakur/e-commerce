df3ebdc9db7a4e081779df4bb0695d7d1aa28735
Shriyasha Thakur <shriyashathakur06@gmail.com>
2024-09-21 00:50:07 +0530

    initial commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p> A classic wardrobe staple, the T-shirt is a short-sleeved, collarless top made from soft, breathable fabrics like cotton or polyester. Known for its comfort and versatility, it's perfect for layering or wearing alone. T-shirts come in a variety of styles, including crew neck, V-neck, and graphic prints, making them suitable for casual outings, sports, or lounging at home.</p>
+        <p>A hoodie is a comfortable, casual sweatshirt with a hood, often featuring a front pocket and a drawstring to adjust the hood. Made from soft materials like cotton or fleece, hoodies are popular for their relaxed fit and streetwear appeal. They are perfect for layering during cooler weather or for lounging at home, and they come in various designs, including zip-up and pullover styles.</p>
+      </div>
+    </div>
+  )
+}

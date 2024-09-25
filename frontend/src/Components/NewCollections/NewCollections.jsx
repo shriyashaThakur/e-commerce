148d5d39797652557a2a594936089fc202621fa1
Shriyasha Thakur <71924070+shriyashaThakur@users.noreply.github.com>
2024-09-25 22:27:35 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ export const NewCollections = () => {
   const [new_collection,setNew_collection] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://e-commerce-backend-nnrd.onrender.com//newcollections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
